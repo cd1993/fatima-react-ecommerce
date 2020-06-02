33236b417a26129c0262caedd93bd4f74c5cc34f
Constantine <cdoukas@redventures.com>
2020-06-02 14:23:40 -0400

    Initial commit - react commerce project

---
+++ README.md
@@ -0,0 +1,13 @@
+Running this app locally:
+    1) Check if you have a node_modules folder in your project. If you do not, run "npm install"
+
+    2) Run the React developer server using "npm start". The project should open at http://localhost:3000
+
+
+
+
+Deploying the app:
+    1) Run "npm run build" to build/compile your React app
+
+    2) Deploy the dist folder to Netlify
+
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Products from './components/Products';
+
+import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+function App() {
+  return (
+        <Router>
+            <div>
+                <header className="navbar">
+                    <nav>
+                        <img className="logo" src="../img/Rg.png" alt="logo" />
+                        <ul className="nav-links">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/products">Products</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                        <div className="burger">
+                            <div className="line1"></div>
+                            <div className="line2"></div>
+                            <div className="line3"></div>
+                        </div>
+                    </nav>
+                </header>
+                <Switch>
+                    <Route path="/"><Home /></Route>
+                    <Route path="/products"><Products /></Route>
+                    <Route path="/contact"><Contact/></Route>
+                </Switch>
+            </div>
+        </Router>
+  );
+}
+
+export default App;
